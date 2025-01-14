a7c0fd34bfc1d5ab08d24e98d81799ccf4ae1b8c
gabtor7 <gabey.toreilles@gmail.com>
2025-01-14 02:58:53 +0100

    init laravel and added vue front

---
+++ database/seeders/TaskSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use app\Models\Tasks;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TaskSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
