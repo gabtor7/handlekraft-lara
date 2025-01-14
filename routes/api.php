798de89ef03a3c1e5df8627f591963045c52966a
gabtor7 <gabey.toreilles@gmail.com>
2025-01-14 23:04:13 +0100

    defined task routes

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/task', [TaskController::class, 'index']);
+
+Route::get('/task/{id}', [TaskController::class, 'show']);
+
+Route::post('/task', [TaskController::class, 'store']);
+
+Route::put('/task/{id}', [TaskController::class, 'update']);
+
+Route::delete('/task/{id}', [TaskController::class, 'delete']);
